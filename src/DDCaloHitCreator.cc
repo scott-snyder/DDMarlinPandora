facb993b568a83ad38dcdeed29ebf611fb828b43
scott snyder <sss@karma>
2021-01-15 15:28:08 -0500

    Avoid error messages for plug hits.

--- src/DDCaloHitCreator.cc
+++ src/DDCaloHitCreator.cc
@@ -357,6 +357,7 @@ pandora::StatusCode DDCaloHitCreator::CreateMuonCaloHits(const EVENT::LCEvent *c
                     {
                       std::cout<<"BIG WARNING: CANNOT HANDLE PLUG HITS (no plug in CLIC model), DO NOTHING!"<<std::endl;
 //                         this->GetEndCapCaloHitProperties(pCaloHit, plugLayers, caloHitParameters, absorberCorrection);
+                      continue;
                     }
                     else if (isInBarrelRegion)
                     {

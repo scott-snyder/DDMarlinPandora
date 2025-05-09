10f5e0c8ea04f98294b898f7979174c9e011f6c5
sss <sss@karma>
2025-05-09 15:35:43 -0400

    Fix clang warnings.
    Unused variables.

--- src/DDCaloDigi.cc
+++ src/DDCaloDigi.cc
@@ -665,22 +665,22 @@ void DDCaloDigi::processEvent(LCEvent* evt) {
             std::vector<bool> used(n, false);
             // for(unsigned int i =0; i<n;i++) used[i] = false;
 
-            int count = 0;
-            float eCellInTime = 0.;
-            float eCellOutput = 0.;
+            //int count = 0;
+            //float eCellInTime = 0.;
+            //float eCellOutput = 0.;
 
             for (unsigned int i_t = 0; i_t < n; i_t++) {
               float timei = hit->getTimeCont(i_t);
               float energyi = hit->getEnergyCont(i_t);
               float energySum = 0;
 
-              float deltat = 0;
-              if (_ecalCorrectTimesForPropagation)
-                deltat = dt;
-              if (timei - deltat > _ecalTimeWindowMin && timei - deltat < ecalTimeWindowMax) {
-                float ecor = energyi * calibr_coeff;
-                eCellInTime += ecor;
-              }
+              // float deltat = 0;
+              // if (_ecalCorrectTimesForPropagation)
+              //   deltat = dt;
+              // if (timei - deltat > _ecalTimeWindowMin && timei - deltat < ecalTimeWindowMax) {
+              //   float ecor = energyi * calibr_coeff;
+              //   eCellInTime += ecor;
+              // }
 
               if (!used[i_t]) {
                 // merge with other hits?
@@ -746,7 +746,7 @@ void DDCaloDigi::processEvent(LCEvent* evt) {
                     timeCor = dt;
                   timei = timei - timeCor;
                   if (timei > _ecalTimeWindowMin && timei < ecalTimeWindowMax) {
-                    count++;
+                    //count++;
                     CalorimeterHitImpl* calhit = new CalorimeterHitImpl();
                     if (_ecalGapCorrection != 0) {
                       _calHitsByStaveLayer[stave][layer].push_back(calhit);
@@ -761,7 +761,7 @@ void DDCaloDigi::processEvent(LCEvent* evt) {
                       // calhit->setEnergy(energyi);
                     }
 
-                    eCellOutput += energyi * calibr_coeff;
+                    //eCellOutput += energyi * calibr_coeff;
 
                     calhit->setTime(timei);
                     calhit->setPosition(hit->getPosition());
@@ -911,7 +911,7 @@ void DDCaloDigi::processEvent(LCEvent* evt) {
 
             std::vector<bool> used(n, false);
 
-            int count = 0;
+            //int count = 0;
 
             for (unsigned int i_t = 0; i_t < n; i_t++) { // loop over all subhits
               float timei = hit->getTimeCont(i_t);       // absolute hit timing of current subhit
@@ -994,7 +994,7 @@ void DDCaloDigi::processEvent(LCEvent* evt) {
                   if (timei > _hcalTimeWindowMin &&
                       timei < hcalTimeWindowMax) { // if current subhit timecluster is within specified timing window,
                                                    // create new CalorimeterHit and add to collections etc.
-                    count++;
+                    //count++;
                     CalorimeterHitImpl* calhit = new CalorimeterHitImpl();
                     calhit->setCellID0(cellid);
                     calhit->setCellID1(cellid1);
@@ -1577,13 +1577,6 @@ LCCollection* DDCaloDigi::combineVirtualStripCells(LCCollection* col, bool isBar
   // loop over input collection
   int numElements = col->getNumberOfElements();
 
-  // // sum energy for check
-  float tempenergysum(0);
-  for (int j(0); j < numElements; ++j) {
-    SimCalorimeterHit* hit = dynamic_cast<SimCalorimeterHit*>(col->getElementAt(j));
-    tempenergysum += hit->getEnergy();
-  }
-
   float scTVirtLengthBar(-99);
   float scLVirtLengthBar(-99);
   float scTVirtLengthEnd(-99);
@@ -1713,12 +1706,10 @@ LCCollection* DDCaloDigi::combineVirtualStripCells(LCCollection* col, bool isBar
     // effect of absorbtion length within scintillator
     //     TODO: should check the polarity is consistent with mppc position, to make sure larger response nearer to
     //     mppc....
-    float energy_new = hit->getEnergy();
 
     float energyNonuniformityScaling(1.);
     if (_strip_abs_length > 0) {
       energyNonuniformityScaling = exp(-relativePos / _strip_abs_length);
-      energy_new *= energyNonuniformityScaling;
     }
 
     // create a new hit for the strip
@@ -1844,16 +1835,9 @@ LCCollection* DDCaloDigi::combineVirtualStripCells(LCCollection* col, bool isBar
     }
 
     // add the MC contriutions
-    float eadd(0);
     for (int ij = 0; ij < hit->getNMCContributions(); ij++) {
       newhit->addMCParticleContribution(hit->getParticleCont(ij), hit->getEnergyCont(ij) * energyNonuniformityScaling,
                                         hit->getTimeCont(ij), hit->getPDGCont(ij));
-      eadd += hit->getEnergyCont(ij) * energyNonuniformityScaling;
-    }
-
-    float esum(0);
-    for (int ij = 0; ij < newhit->getNMCContributions(); ij++) {
-      esum += newhit->getEnergyCont(ij);
     }
   } // loop over hits
 
